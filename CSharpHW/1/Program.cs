9c347eb3e760433453d7b52b9c5aa32e82fee402
Alex Buzskiy <buzskiyalexandr@gmail.com>
2019-12-05 21:51:31 +0200

    task 1 console app + task 2 console app calc

---
+++ CSharpHW/1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Yellow;
+            var name = "Alex";
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
---
+++ CSharpHW/2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose desired operation. 1 - Addition, 2 - Substraction, 3 - Multiplication, 4 - Division");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            double result = 0.0;
+            switch (input3)
+            {
+                case 1:
+                result = input1 + input2;
+                break;
+
+                case 2:
+                result = input1 - input2;
+                break;
+
+                case 3:
+                result = input1 * input2;
+                break;
+
+                case 4:
+                result = input1 / input2;
+                break;
+            }
+
+            Console.WriteLine($"Result of your calculation is {Math.Round(result,2)}");
+        }
+    }
+}
