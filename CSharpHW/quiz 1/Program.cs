9e4d65ce66ba15294aa3647ed60a4cf39971180d
Alex Buzskiy <buzskiyalexandr@gmail.com>
2019-12-08 19:38:05 +0200

    task #1 from quiz

---
+++ CSharpHW/quiz 1/Program.cs
@@ -0,0 +1,36 @@
+﻿//3.1 Метод в качестве аргумента принимает натуральное число и выводит в консоль все делители аргумента.
+
+using System;
+
+namespace quiz_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserNumber = 0;
+            Console.WriteLine("Please enter number:");
+            while (true)
+                {
+                    try
+                        {
+                            UserNumber = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        }
+                    catch (Exception)
+                        {
+                            Console.WriteLine("Wrong Input Format");
+                            continue;
+                        }
+                }
+                    for(int i=1; i<=UserNumber; i++)
+                        {
+                            if (UserNumber%i==0)
+                                {
+                                    Console.Write(i + " ");
+                                }
+                        }
+                    Console.WriteLine("\n");
+        }
+    }
+}
