ae150bce43264a81ac172d6fd27df9ca84d19ae4
Alex Buzskiy <buzskiyalexandr@gmail.com>
2019-12-08 23:17:43 +0200

    task 7.1 completed

---
+++ CSharpHW/7.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              var Car = new Car();
+              Car.Color = "Blue";
+            Console.WriteLine($"Car color is {Car.Color}");
+            TuningAtelier.TuneCar(Car);
+        }
+    }
+
+    class Car 
+    {
+        public string Model {get;set;}
+        public string Year {get;set;}
+        public string Color {get;set;}
+    }
+    static class TuningAtelier
+    {
+        public static void TuneCar(Car Car)
+        {
+            Car.Color = "Red";
+            Console.WriteLine($"Car color is changed to {Car.Color}");   
+        }
+    }
+}
