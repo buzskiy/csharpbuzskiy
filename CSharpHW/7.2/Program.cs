00405e19b79e8f0b57c740b9822dfe1c1983e353
Alex Buzskiy <buzskiyalexandr@gmail.com>
2019-12-09 22:05:44 +0200

    7.2 and 7.3 tasks

---
+++ CSharpHW/7.2/Program.cs
@@ -0,0 +1,53 @@
+﻿/**Создайте консольное приложение. Создайте класс Human. 
+   Клас должен содержать следующие свойства: BirthDate, FirstName, Lastame, Age.
+   Свойство Age должно быть только для чтения. Класс Human должен иметь несколько конструкторов.
+   Класс должен иметь метод проверяющий текущий екземпляр на еквивалентность с другим. */
+
+using System;
+
+namespace _7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Alex = new Human();
+            Human Borys = new Human("Borys", "Ivanov");
+            Human Borys2 = new Human ("Borys", "Ivanov");
+            Human Ivan = new Human("Ivan","Ivanov");
+            Human.HumanEqual(Borys,Borys2);
+        }
+    }
+
+    class Human
+    {
+        public string BirthDate {get;set;}
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public int Age {get;} = 29;
+
+        public  Human() {}
+        public  Human(string FirstName)
+        {
+            this.FirstName=FirstName;
+        } 
+        public  Human(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public static void HumanEqual (Human human1, Human human2)
+        {
+            if (human1.FirstName==human2.FirstName&&human1.LastName==human2.LastName&&human1.BirthDate==human2.BirthDate)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+
+    }
+}
---
+++ CSharpHW/7.3/Program.cs
@@ -0,0 +1,62 @@
+﻿/**Создайте класс ShapeDescriptor а также Point (описывает координаты).
+   Класс ShapeDescriptor должен имень несколько конструкторов с различным количеством точек.
+   Класс ShapeDescriptor должен иметь свойство ShapeType определяющее тип фигуры. */
+
+using System;
+
+namespace _7._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var point1 = new Point(3,5);
+            var point2 = new Point (4,6);
+            var point3 = new Point (5,7);
+
+            var shape1 = new ShapeDescriptor(point1);
+            var shape2 = new ShapeDescriptor(point1, point2);
+            var shape3 = new ShapeDescriptor(point1, point2, point3);
+
+            ShapeDescriptor.ShapeType(shape1);
+            ShapeDescriptor.ShapeType(shape2);
+            ShapeDescriptor.ShapeType(shape3);      
+        }
+    }
+    class Point
+    {
+        public int x = 0;
+        public int y = 0;
+        public Point (int x, int y)
+        {
+
+        }
+    }
+
+    class ShapeDescriptor
+    {
+        public string shape
+            {get; set;}
+        public ShapeDescriptor(Point a)
+        {
+            this.shape = "Dot";
+        }
+
+        public ShapeDescriptor(Point a, Point b)
+        {
+            this.shape = "Line";
+        }
+
+        public ShapeDescriptor(Point a, Point b, Point c)
+        {
+            this.shape = "Triangle";
+        }
+
+        public static void ShapeType(ShapeDescriptor inputshape)
+        {
+            
+            Console.WriteLine(inputshape.shape);
+        }
+    }
+}
