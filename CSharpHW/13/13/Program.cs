4bff1026906ab85c60b92579d43a9b8c93afe1f7
Alex Buzskiy <buzskiyalexandr@gmail.com>
2020-01-03 16:12:18 +0200

    task 13 completed

---
+++ CSharpHW/13/13/Program.cs
@@ -0,0 +1,59 @@
+﻿//Создайте консольное приложение.Постойте иерархию наследования ResourceHolderBase, ResourceHolderDerived. Реализуйте IDisposable для каждого из них.
+//Продемонстрируйте вызов Dispose каждого из них.Реализуйте деструктор, убедитесь что ресурсы будут освободжены если деструктор отработает ранее,
+//убедитесь что повторый вызов Dispose не вызывает исключительные ситуации.
+   
+
+using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ResourceHolderBase n1 = new ResourceHolderBase())
+            {
+            }
+            using (ResourceHolderDerived n2 = new ResourceHolderDerived())
+            {
+            }
+            ResourceHolderBase n3 = new ResourceHolderBase();
+            n3.Dispose();
+            ResourceHolderDerived n4 = new ResourceHolderDerived();
+            n4.Dispose();
+            Console.WriteLine("End of program");
+            
+            
+
+        }
+    }
+
+    public class ResourceHolderBase : IDisposable
+    {
+        public ResourceHolderBase()
+        {
+            Console.WriteLine("Object being created");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Object being disposed");
+        }
+
+        ~ResourceHolderBase()
+        {
+            Console.WriteLine("Object being finalized");
+            Dispose();
+        }
+
+    }
+
+    public class ResourceHolderDerived : ResourceHolderBase
+    {
+
+        public new void Dispose()
+        {
+            Console.WriteLine("Derived Object being disposed");
+        }
+    }
+}
